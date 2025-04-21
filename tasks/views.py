2f35b61c0685530b459779ac5d53af7888aa8f31
Arsheen14-sh <arsh.shaikh1612@gmail.com>
2025-04-21 11:22:36 +0530

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+
+# Create your views here.
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'create_task.html', {'form': form})
+
+def task_list(request):
+    sort=request.GET.get('sort')
+    if sort == 'due_date':
+        tasks = tasks.order_by('due_date')
+    else:
+        tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def update_task(request, id):
+    task = Task.objects.get(id=id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, request.FILES, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'create_task.html', {'form': form})
+
+def delete_task(request, id):
+    post = Task.objects.get(id=id)
+    post.delete()
+    return redirect('task_list')
